c9aaa34105dae1df08fe908d0e55cf4381f375e7
Johan Sageryd <j416@1616.se>
2011-05-31 09:23:39 +0200

    Update YAML example

--- yaml.rb
+++ yaml.rb
@@ -3,5 +3,18 @@
 
 require 'yaml'
 
-p yaml = [:foo, :bar, :baz].to_yaml
-p array = YAML.load(yaml)
+p array = [:foo, :bar, :baz]
+
+# String
+p yaml = array.to_yaml
+p loaded = YAML.load(yaml)
+
+# File save/load
+filename = 'my_file.yaml'
+File.open(filename, 'w') do |io|
+	YAML.dump(array, io)
+end
+loaded = YAML.load_file(filename)
+
+# Clean up
+File.delete(filename)
