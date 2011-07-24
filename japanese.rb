ce92ca8f40f4be0c207cd2342d6a0b5184177854
Johan Sageryd <j416@1616.se>
2011-07-24 22:55:00 +0200

    Update Japanese content checker example

--- japanese.rb
+++ japanese.rb
@@ -13,6 +13,13 @@ def containsJapanese?(text)
   !!(text =~ /[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FC2\uFF61-\uFF9D\u31F0-\u31FF\u3000-\u303F]/)
 end
 
+def containsHiragana?(text)
+  # 3040-309F hiragana
+  #
+  # Source: http://www.unicode.org/charts/
+  !!(text =~ /[\u3040-\u309F]/)
+end
+
 def containsKatakana?(text)
   # 30A0-30FF katakana
   # FF61-FF9D half-width katakana
@@ -22,8 +29,37 @@ def containsKatakana?(text)
   !!(text =~ /[\u30A0-\u30FF\uFF61-\uFF9D\u31F0-\u31FF]/)
 end
 
-puts 'Japanese | Katakana | Input'
-puts '--------------------------------'
-['Hello', 'こんにちは', 'コンニチハ', 'ｺﾝﾆﾁﾊ'].each do |s|
-  puts "   #{containsJapanese?(s) ? 'YES' : 'NO '}        #{containsKatakana?(s) ? 'YES' : 'NO '}     #{s}"
+def containsKanji?(text)
+  # 4E00-9FC2 kanji
+  #
+  # Source: http://www.unicode.org/charts/
+  !!(text =~ /[\u4E00-\u9FC2]/)
 end
+
+def containsCJKPunctuation?(text)
+  # 3000-303F CJK punctuation
+  #
+  # Source: http://www.unicode.org/charts/
+  !!(text =~ /[\u3000-\u303F]/)
+end
+
+def c(value)
+  color = value ? 34 : 31
+  text = value ? 'Ｏ' : 'Ｘ'
+  "\e[##{color}m#{text}\e[0m"
+end
+
+puts "和あア漢点"
+  ['Hello',
+   'Ｈｅｌｌｏ',
+   'こんにちは',
+   'パクる',
+   'ド真ん中',
+   '「ブ暑い」',
+   'ありがとう',
+   'アリガトウ',
+   '孑孒',
+   '々。、',
+   '有り難う',
+   'ｱﾘｶﾞﾄｳ',
+   '・'].each { |s| puts "#{c containsJapanese?(s)}#{c containsHiragana?(s)}#{c containsKatakana?(s)}#{c containsKanji?(s)}#{c containsCJKPunctuation?(s)}#{s}" }
